bd9490e76ebd4e90788d8e007fad399bbcebc6e7
Juuso Pyykkönen <juuso.pyyk@gmail.com>
2024-04-17 19:44:15 +0300

    Change validation to be more precise - exercise 3.20

--- models/person.ts
+++ models/person.ts
@@ -18,9 +18,21 @@ mongoose.connect(url)
         console.log('error connecting to MongoDB:', error.message)
     })
 
+const numberValidator = (num: string) => {
+    return /^\d{2,3}-\d{6,7}$/.test(num)
+}
+
 const personSchema = new mongoose.Schema<Omit<PersonType, 'id'>>({
-    name: { type: String, required: true, minlength: 3 },
-    number: { type: String, required: true },
+    name: {
+        type: String,
+        required: true,
+        minlength: 3,
+    },
+    number: {
+        type: String,
+        required: true,
+        validate: { validator: numberValidator }
+    },
 })
 
 personSchema.set('toJSON', {
@@ -29,8 +41,8 @@ personSchema.set('toJSON', {
     transform: (_document, returnedObject) => {
         delete returnedObject._id
     }
-}
-)
+})
+
 
 
 const Person = mongoose.model<Omit<PersonType, 'id'>>('Person', personSchema)
